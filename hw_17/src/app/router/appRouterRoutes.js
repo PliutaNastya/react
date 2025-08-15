b3f18af600059b706396b71ab413c8f9a12c8271
Anastasiia <koicheva1996@gmail.com>
2025-08-16 00:45:00 +0300

    fix: routes

--- hw_17/src/app/router/appRouterRoutes.js
+++ hw_17/src/app/router/appRouterRoutes.js
@@ -1,10 +1,14 @@
-import { frontRoutes } from '@/shared/config/routes/frontRoutes'
+import { frontRoutes } from "@/shared/config/routes/frontRoutes"
+
+// Створюємо мапу всіх сторінок
+const pagesModules = import.meta.glob('../../pages/*.jsx', { eager: false })
 
 const pagesList = Object.keys(frontRoutes.pages)
 
 export const appRouterRoutes = pagesList.map((page) => ({
-  ...frontRoutes.pages[page],
-  lazy: async () => ({
-    Component: (await import(`../../pages/${page}`)).default,
-  }),
+	...frontRoutes.pages[page],
+	lazy: async () => {
+		const mod = await pagesModules[`../../pages/${page}.jsx`]() // ← строго за ключем
+		return { Component: mod.default }
+	},
 }))
